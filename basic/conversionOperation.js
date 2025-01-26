6a41ed91981862a7c35c18fe2690662d5a601840
Virajj1304 <sawantviraj856@gmail.com>
2025-01-26 06:44:58 +0000

    conversion of data types

---
+++ basic/conversionOperation.js
@@ -0,0 +1,30 @@
+//let score = 
+//console.log(typeof score ); 
+//console.log((typeof score ));
+
+//let valueInNumber = Number(score)
+//console.log((typeof valueInNumber ));
+//console.log(valueInNumber);
+
+// a string no. can be converted easily  eg. "33"== 33
+//  "23abc" cannot be converted thats y it shows output as NAN(not a number)
+// boolean function i.e true == 1 and false==0 will be converted elsewise
+
+
+//let isloggedin = "viraj"
+
+//let booleanisloggedin = Boolean ( isloggedin)
+ //console.log (booleanisloggedin);
+
+ // boolean 1 == true
+ // 0 is false 
+ // empty string "" ====  false 
+ // "viraj"  == true
+
+
+ let someNumber = 23 
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log (typeof stringNumber)
+
+ 
