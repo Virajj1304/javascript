55aebb05cabb1e68054a3b04c2baf73f924a57be
Virajj1304 <sawantviraj856@gmail.com>
2025-01-27 17:40:41 +0000

    comparision

---
+++ basic/comparision.js
@@ -0,0 +1,21 @@
+/* console.log(2>1)
+console.log(2>=1) */
+
+
+/* console.log("2">2)
+console.log("02">1) */
+
+console.log(null==0)
+console.log(null>0)
+console.log(null>=0)
+
+// an equality check  == and comparisons >< >= <= work differently
+// comparisons convert null no. to a no. treating it as zero 
+
+console.log (undefined==0)
+
+// ===
+
+console.log(2=== 2)               // triple equals (===) gives strict check and does not do conversions
+
+
