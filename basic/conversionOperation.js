959c781e25ecb825913dc29afde466333000007c
Virajj1304 <sawantviraj856@gmail.com>
2025-01-27 16:21:40 +0000

    operations

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -24,7 +24,37 @@
 
  let someNumber = 23 
  let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log (typeof stringNumber)
+ //console.log(stringNumber);
+ //console.log (typeof stringNumber)
 
- 
+  // ************************************** operations**************************************//
+
+let value = 32
+let negvalue = -value
+//console.log(negvalue)
+
+let str1 = "hello"
+let str2= " viraj"
+
+let str3 = str1+str2
+
+/* console.log(str3)
+console.log(viraj)
+console.log(aww)
+onsole.log("1"+3)                        
+console.log (1+"3")
+console.log ("1"+"3")
+console.log(1+2+"4")
+ */
+//console.log((3+4)*6%6)
+
+console.log(-true)
+console.log(+"")
+
+let num1, num2 ,num3
+
+num1= num2=num3=2+2
+
+let gamecounter= 100
+++gamecounter
+console.log(gamecounter)
