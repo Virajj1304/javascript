d3d07012f1f0f5f28608c186052dcad2c8a7e77f
Virajj1304 <sawantviraj856@gmail.com>
2025-01-29 16:45:19 +0000

    summary

---
+++ basic/datatypes-summary.js
@@ -0,0 +1,13 @@
+// Primitive
+// 7 types : String, number , boolean , null , undefined , symbol, bigint 
+
+// Reference ( non primitive)
+
+const score = 100
+const scorevalue = 100.4
+
+const isloggedin = false
+
+
+
+// arrays , objects , functions 
